5d4fc6a2617adaed42e103f7e41d336a6dcfbdfa
sagarsagat <sagarsagat0@gmail.com>
2022-07-17 00:34:01 +0530

    17

--- index.php
+++ index.php
@@ -27,13 +27,16 @@ session_start();
 
     <title>Notesify</title>
     <script src="js/jquery.min.js"></script>
-    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
     <script src="js/owl.carousel.min.js"></script>
     <script src="js/app.js"></script>
-  
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+
+   
   
   <script>$( document ).ready( function() {
-    $('#modalAbandonedCart').modal('show');
+    $('#myModal').modal('show');
+    
     
     });
 </script>
@@ -422,65 +425,37 @@ session_start();
 
 
     
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered modal-xl">
-            <div class="modal-content">
-                <div class="modal-body p-0">
-                    <div class="container-fluid">
-                        <div class="row gy-4">
-                            <div class="col-lg-4 col-sm-12 bg-cover"
-                                style="background-image: url(img/c2.jpg); min-height:300px;">
-                                <div>
-                                    
-                                </div>
-                            </div>
-                            <div class="col-lg-8">
-                                <form class="p-lg-5 col-12 row g-3">
-                                    <div>
-                                        <h1>Get in touch</h1>
-                                    <p>Fell free to contact us and we will get back to you as soon as possible</p>
-                                    </div>
-                                    <div class="col-lg-6">
-                                        <label for="userName" class="form-label">First name</label>
-                                        <input type="text" class="form-control" placeholder="Jon" id="userName"
-                                            aria-describedby="emailHelp">
-                                    </div>
-                                    <div class="col-lg-6">
-                                        <label for="userName" class="form-label">Last name</label>
-                                        <input type="text" class="form-control" placeholder="Doe" id="userName"
-                                            aria-describedby="emailHelp">
-                                    </div>
-                                    <div class="col-12">
-                                        <label for="userName" class="form-label">Email address</label>
-                                        <input type="email" class="form-control" placeholder="Johndoe@example.com" id="userName"
-                                            aria-describedby="emailHelp">
-                                    </div>
-                                    <div class="col-12">
-                                        <label for="exampleInputEmail1" class="form-label">Enter Message</label>
-                                        <textarea name="" placeholder="This is looking great and nice." class="form-control" id=""  rows="4"></textarea>
-                                    </div>
-
-                                    <div class="col-12">
-                                        <button type="submit" class="btn btn-brand">Send Message</button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    </div>
     
-
+   
+  
+    <div class="modal" id="myModal" >
+    <div class="modal-dialog">
+      <div class="modal-content" >
+      
+        <!-- Modal Header -->
+       <div class="modal-header" id="nd" >
+          <h4 class="modal-title" id="nd">Current News and Annoccement</h4>
+          <button type="button" class="btn-close" data-dismiss="modal" aria-hidden="true" aria-label="Close"></button>
+        </div>
+        
+        <!-- Modal body -->
+        <div class="modal-body" id="lk">
+         <center><a href="" style="color:red;" id="hq" >--------sample notification-------- </a></center><br><br>
+         <center><a href="" style="color:red;">--------sample notification--------</a></center><br><br>
+         <center><a href="" style="color:red;">--------sample notification--------</a></center><br><br>
+         <center><a href="" style="color:red;">--------sample notification--------</a></center><br><br>
+         <center><a href="" style="color:red;">--------sample notification--------</a></center><br><br>
+         
+        </div>
+        
+        <!-- Modal footer -->
+        
+        
+      </div>
+    </div>
+  </div>
   
 
-
-
-
-
-   
 </body>
 
 </html>
