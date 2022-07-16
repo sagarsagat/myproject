99f1383a83a2b05c6c5df3477a38bd8705845e24
sagarsagat <sagarsagat0@gmail.com>
2022-07-16 23:27:40 +0530

    17

--- index.php
+++ index.php
@@ -421,7 +421,7 @@ session_start();
     </footer>
 
 
-    <!-- Modal -->
+    
     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered modal-xl">
             <div class="modal-content">
@@ -477,47 +477,6 @@ session_start();
   
 
 
-
-<!-- Modal: modalAbandonedCart-->
-<div class="modal fade right" id="modalAbandonedCart" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
-  aria-hidden="true" data-backdrop="false">
-  <div class="modal-dialog modal-side modal-bottom-right modal-notify modal-info" role="document">
-    <!--Content-->
-    <div class="modal-content">
-      <!--Header-->
-      <div class="modal-header">
-        <p class="heading">Product in the cart
-        </p>
-
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-      </div>
-
-      <!--Body-->
-      <div class="modal-body">
-
-        <div class="row">
-          <div class="col-3">
-            <p></p>
-            <p class="text-center"><i class="fas fa-shopping-cart fa-4x"></i></p>
-          </div>
-
-          <div class="col-9">
-            <p>Do you need more time to make a purchase decision?</p>
-            <p>No pressure, your product will be waiting for you in the cart.</p>
-          </div>
-        </div>
-      </div>
-
-      <!--Footer-->
-      <div class="modal-footer justify-content-center">
-        <a type="button" class="btn btn-info">Go to cart</a>
-        <a type="button" class="btn btn-outline-info waves-effect" data-dismiss="modal">Cancel</a>
-      </div>
-    </div>
-    <!--/.Content-->
-  </div>
-</div>
-<!-- Modal: modalAbandonedCart-->
 
 
 
